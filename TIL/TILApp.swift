f8cfb2ec84591e68e945a201bd21f5d31126c974
vincio <airfoil_elector0b@icloud.com>
2021-08-29 16:31:20 +0800

    Initial Commit

---
+++ TIL/TILApp.swift
@@ -0,0 +1,17 @@
+//
+//  TILApp.swift
+//  TIL
+//
+//  Created by Vincio on 8/29/21.
+//
+
+import SwiftUI
+
+@main
+struct TILApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

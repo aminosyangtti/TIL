921d19a4c09dc3a168cdb244e745f670328616ae
vincio <airfoil_elector0b@icloud.com>
2021-08-31 02:14:07 +0800

    added User model and  sessionstore view model

---
+++ TIL/Models/User.swift
@@ -0,0 +1,14 @@
+//
+//  User.swift
+//  User
+//
+//  Created by Vincio on 8/30/21.
+//
+
+import Foundation
+
+struct User: Hashable{
+    var uid: String
+    var email: String
+
+}
---
+++ TIL/ViewModels/SessionStore.swift
@@ -0,0 +1,76 @@
+//
+//  SessionStore.swift
+//  SessionStore
+//
+//  Created by Vincio on 8/30/21.
+//
+
+import Foundation
+
+class SessionStore: ObservableObject {
+    @Published var session: User?
+    @Published var isAnon: Bool = false
+    var handle: AuthStateDidChangeListenerHandle?
+    let authRef = Auth.auth()
+
+    func listen() {
+        handle = authRef.addStateDidChangeListener({(auth, user) in
+            if let user = user {
+                self.isAnon = false
+                self.session = User(uid: user.uid, email: user.email!)
+            } else {
+                self.isAnon = true
+                self.session = nil
+            }
+        })
+    }
+
+    func signIn(email: String, password: String) {
+        authRef.signIn(withEmail: email, password: password) { (result, error)  in
+            if error != nil {
+                self.alertMessage = error?.localizedDescription ?? "no error"
+                self.showAlert = true
+
+            } else {
+
+
+            }
+
+        }
+    }
+
+    func signUp(email: String, name: String, password: String) {
+        authRef.createUser(withEmail: email, password: password) { (result, error) in
+            if error != nil {
+                self.alertMessage = error?.localizedDescription ?? "no error"
+                self.showAlert = true
+
+            } else {
+                let user = Auth.auth().currentUser
+
+                UsersViewModel().addUser(email: user!.email!, uid: user!.uid , name: name, handler: {})
+
+            }
+
+        }
+    }
+
+    func signOut() -> Bool {
+        do {
+            try authRef.signOut()
+            self.session = nil
+            self.isAnon = true
+            return true
+        } catch {
+            return false
+        }
+    }
+
+    func unbind() {
+        if let handle = handle {
+            authRef.removeStateDidChangeListener(handle)
+        }
+    }
+
+
+}

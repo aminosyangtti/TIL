44f94bbfeaf0d1bb72dc9d1bed578374db4112f5
vincio <airfoil_elector0b@icloud.com>
2021-08-31 02:42:39 +0800

    removed restrictions to users without account to add bubbles

--- TIL/ViewModels/BubblesViewModel.swift
+++ TIL/ViewModels/BubblesViewModel.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import Firebase
 
 class BreakoutRoomsViewModel: ObservableObject {
     @Published var bubbles = [Bubble]()
@@ -17,43 +18,41 @@ class BreakoutRoomsViewModel: ObservableObject {
     }
 
     func fetchData() {
-        if user != nil {
-            db.collection("bubbles").addSnapshotListener({(snapshot, error) in
-                guard let documents = snapshot?.documents else {
-                    print("no docs")
-                    return
-                }
+        db.collection("bubbles").addSnapshotListener({(snapshot, error) in
+            guard let documents = snapshot?.documents else {
+                print("no docs")
+                return
+            }
 
-                self.bubbles = documents.map({docSnapshot -> Bubble in
-                    let data = docSnapshot.data()
-                    let docId = docSnapshot.documentID
-                    let text = data["text"] as? String ?? ""
-                    let likedBy = data["likedBy"] as? [String] ?? [""]
-                    let likes = data ["likes"] as? Int ?? 0
-                    return Bubble(id: docId, text: text, likedBy: likedBy)
+            self.bubbles = documents.map({docSnapshot -> Bubble in
+                let data = docSnapshot.data()
+                let docId = docSnapshot.documentID
+                let text = data["text"] as? String ?? ""
+                let likedBy = data["likedBy"] as? [String] ?? [""]
+                let likes = data ["likes"] as? Int ?? 0
+                return Bubble(id: docId, text: text, likedBy: likedBy)
 
-                })
             })
-        }
+        })
     }
 
     func createBubble(text: String, handler: @escaping () -> Void) {
-        if user != nil {
-            db.collection("bubbles").addDocument(data:
-                                                        [
-                                                            "text": text,
-                                                            "likes": 0,
-                                                            ]) { err in
-                if let err = err {
-                    print(err)
 
-                } else {
-                    handler()
+        db.collection("bubbles").addDocument(data:
+                                                    [
+                                                        "text": text,
+                                                        "likes": 0,
+                                                        ]) { err in
+            if let err = err {
+                print(err)
 
-                }
+            } else {
+                handler()
 
             }
+
         }
+
     }
 
     func likeBubble(text: String, handler: @escaping () -> Void) {
@@ -95,6 +94,4 @@ class BreakoutRoomsViewModel: ObservableObject {
             }
         }
     }
-
-
 }

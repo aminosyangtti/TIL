8c88471a9c2b190882a26d41d549b2c03d884349
vincio <airfoil_elector0b@icloud.com>
2021-08-31 02:36:47 +0800

    added Bubble model and BubblesViewModel

---
+++ TIL/Models/Bubble.swift
@@ -0,0 +1,15 @@
+//
+//  Bubble.swift
+//  Bubble
+//
+//  Created by Vincio on 8/31/21.
+//
+
+import Foundation
+
+struct Bubble {
+    var id: String
+    var text: String
+    var likedBy: [String]
+    var likes: Int
+}
